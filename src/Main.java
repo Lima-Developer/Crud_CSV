75e425e7bebfe27fd50adaa8e165684c80383945
AnaSouzaPinheiro <anapinheirole@gmail.com>
2024-10-15 19:03:45 -0300

    Adicionando o update do switch

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class Main implements CSV {
     protected static DataBase dataBase = new DataBase();
+    protected static String filePath;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -30,10 +31,11 @@ public class Main implements CSV {
                     // Create Table
                     System.out.print("Informe o nome do arquivo .csv: ");
 
-                    String filePath = scanner.nextLine();
+                    filePath = scanner.nextLine();
                     dataBase = CSV.readCSV(filePath);
                     break;
                 case 2:
+                    // Select Table
                     try {
                         dataBase.selectDataBase("dataBase.txt");
                     } catch (IOException e) {
@@ -45,6 +47,15 @@ public class Main implements CSV {
                     continue;
                 case 4:
                     // Update Register
+                    int rrn;
+                    System.out.print("Insira o número do registro que deseja atualizar: ");
+                    rrn = scanner.nextInt();
+
+                    try {
+                        dataBase.updateRegister(rrn);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
                     continue;
                 case 5:
                     // Delete Register
